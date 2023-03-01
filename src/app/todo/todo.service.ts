ab797cd1c07d7950384a483e97051e64cc2d9f28
Weslei Miranda <wsmiranda11@gmail.com>
2023-03-01 20:30:28 -0300

    feat: implements todo service

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TodoEntity } from './entity/todo.entity';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoRepository: Repository<TodoEntity>,
+  ) {}
+
+  async findAllAsync() {
+    return await this.todoRepository.find();
+  }
+
+  async findOneOrFailAsync(id: string) {
+    try {
+      return await this.todoRepository.findOneByOrFail({ id: id });
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
+
+  async createAsync(data) {
+    return await this.todoRepository.save(this.todoRepository.create(data));
+  }
+
+  async updateAsync(id: string, data) {
+    const todo = await this.findOneOrFailAsync(id);
+    this.todoRepository.merge(todo, data);
+    return await this.todoRepository.save(todo);
+  }
+
+  async deleteByIdAsync(id: string) {
+    await this.findOneOrFailAsync(id);
+    await this.todoRepository.softDelete(id);
+  }
+}
