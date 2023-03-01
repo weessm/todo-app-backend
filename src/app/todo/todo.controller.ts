d15d9db15598d76d7b02529cadf563691b6c374b
Weslei Miranda <wsmiranda11@gmail.com>
2023-03-01 20:50:22 -0300

    feat: implements todo controller

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common/enums';
+import { ParseUUIDPipe } from '@nestjs/common/pipes';
+import { TodoService } from './todo.service';
 
 @Controller('api/v1/todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  async index() {
+    return await this.todoService.findAllAsync();
+  }
+
+  @Post()
+  async create(@Body() body) {
+    return await this.todoService.createAsync(body);
+  }
+
+  @Get(':id')
+  async show(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.todoService.findOneOrFailAsync(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body) {
+    return await this.todoService.updateAsync(id, body);
+  }
+
+  @Delete()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.todoService.deleteByIdAsync(id);
+  }
+}
