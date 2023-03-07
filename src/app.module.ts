b984effb808312dd2b0ea32200cef2b446ef3914
Weslei Miranda <wsmiranda11@gmail.com>
2023-03-07 01:20:33 -0300

    fix: forces recognition of entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoEntity } from './app/todo/entity/todo.entity';
 import { TodoModule } from './app/todo/todo.module';
 
 @Module({
@@ -15,7 +16,7 @@ import { TodoModule } from './app/todo/todo.module';
         username: configServive.get('DB_USERNAME', 'root'),
         password: configServive.get('DB_PASSWORD', ''),
         database: configServive.get('DB_DATABASE', 'todoapp'),
-        entities: [__dirname + '/**/*.entity{.js,.ts}'],
+        entities: [__dirname + '/**/*.entity{.js,.ts}', TodoEntity],
         synchronize: true,
       }),
     }),
