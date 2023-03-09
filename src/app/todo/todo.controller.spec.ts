5518e06d7a8c4a36ae76cdcafbce639d0787a7ef
Weslei Miranda <wsmiranda11@gmail.com>
2023-03-09 00:21:46 -0300

    test: return and exception in TodoController index

--- src/app/todo/todo.controller.spec.ts
+++ src/app/todo/todo.controller.spec.ts
@@ -1,6 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TodoEntity } from './entity/todo.entity';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
+
+const todoEntityList: TodoEntity[] = [
+  new TodoEntity({ id: '1', task: 'task-1', isDone: 1 }),
+  new TodoEntity({ id: '2', task: 'task-2', isDone: 0 }),
+  new TodoEntity({ id: '3', task: 'task-3', isDone: 1 }),
+];
 
 describe('TodoController', () => {
   let todoController: TodoController;
@@ -13,7 +20,7 @@ describe('TodoController', () => {
         {
           provide: TodoService,
           useValue: {
-            findAllAsync: jest.fn(),
+            findAllAsync: jest.fn().mockResolvedValue(todoEntityList),
             createAsync: jest.fn(),
             findOneOrFailAsync: jest.fn(),
             updateAsync: jest.fn(),
@@ -30,5 +37,23 @@ describe('TodoController', () => {
   it('should be defined', () => {
     expect(todoController).toBeDefined();
     expect(todoService).toBeDefined();
+  });
+
+  describe('index', () => {
+    it('should return a todo list entity successfully', async () => {
+      const result = await todoController.index();
+
+      expect(result).toEqual(todoEntityList);
+      expect(typeof result).toEqual('object');
+      expect(todoService.findAllAsync).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an exception', () => {
+      jest
+        .spyOn(todoService, 'findAllAsync')
+        .mockRejectedValueOnce(new Error());
+
+      expect(todoController.index()).rejects.toThrowError();
+    });
   });
 });
